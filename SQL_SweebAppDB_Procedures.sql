c90049756e41cba54f6af8551b954f525a67ef36
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2026-02-09 21:43:55 +0200

    # Create Stored Procedures and related documentation

---
+++ SQL_SweebAppDB_Procedures.sql
@@ -0,0 +1,156 @@
+USE SweebDataBase;
+GO;
+
+CREATE PROCEDURE registerUser
+    @Username NVARCHAR(255),
+	@Email NVARCHAR(255),
+	@PasswordHash NVARCHAR(255),
+	@Success INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	BEGIN TRY
+	    BEGIN TRANSACTION;
+		
+		INSERT INTO dbo.User_Info (Username , Email , PasswordHash , LastLogin ) 
+		VALUES (@Username , @Email , @PasswordHash , GETDATE());
+	
+		DECLARE @UserId INT = CAST(SCOPE_IDENTITY() AS INT);
+		
+		INSERT INTO dbo.UserSettings (UserId) VALUES (@UserId);
+
+		COMMIT;
+		SET @Success = 1;
+	END TRY
+	BEGIN CATCH
+	    IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+        SET @Success = 0;
+	END CATCH
+END
+GO;
+
+
+CREATE PROCEDURE loginUser
+    @Username NVARCHAR(255),
+	@PasswordHash NVARCHAR(255),
+	@Success INT OUTPUT,
+	@UserId INT OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	SELECT TOP 1 @UserId = IdUser
+	FROM dbo.User_Info
+	WHERE (Username = @Username OR Email = @Username) 
+	  AND PasswordHash = @PasswordHash;
+
+
+	IF @UserId IS NOT NULL
+	   SET @Success = 1;
+	ELSE 
+	   SET @Success = 0;
+	
+END
+GO;
+
+
+CREATE PROCEDURE addDevice
+	@Name NVARCHAR(255),
+	@OS NVARCHAR(255),
+	@UserId INT
+AS
+BEGIN 
+    SET NOCOUNT ON;
+
+    IF NOT EXISTS (SELECT 1 FROM dbo.Devices WHERE Name = @Name AND UserId = @UserId)
+	BEGIN
+	   INSERT INTO dbo.Devices (Name , OS, UserId) VALUES (@Name , @OS, @UserId)
+	END
+END
+GO;
+
+
+CREATE PROCEDURE addRule
+	@UserId INT,
+	@RuleName NVARCHAR(255),
+	@IsEnabled BIT,
+	@Priority INT,
+	@Action NVARCHAR(255),
+	@MatchType NVARCHAR(50),
+	@Pattern NVARCHAR(255)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF NOT EXISTS (SELECT 1 FROM dbo.Rules WHERE Name = @RuleName AND UserId = @UserId)
+	BEGIN
+		INSERT INTO dbo.Rules (UserId , Name , IsEnabled , Priority , Action , MatchType , Pattern ) 
+		VALUES (@UserId , @RuleName , @IsEnabled , @Priority , @Action , @MatchType , @Pattern)
+	END
+END
+GO;
+
+CREATE PROCEDURE addThreatEvent 
+	@URL NVARCHAR(2048),
+	@Protocol NVARCHAR(10),
+	@Host NVARCHAR(255),
+	@Path NVARCHAR(255),
+	@Status NVARCHAR(50),
+	@Verdict NVARCHAR(50),
+	@ActionTaken NVARCHAR(255),
+	@Score INT,
+	@Category NVARCHAR(255),
+	@DeviceId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF NOT EXISTS (SELECT 1 FROM dbo.ThreatEvents WHERE URL = @URL AND DeviceId = @DeviceId AND Status = @Status AND Verdict = @Verdict)
+	BEGIN
+		INSERT INTO dbo.ThreatEvents (URL , Protocol , Host , Path , Status , Verdict , ActionTaken , Score , Category , DeviceId) 
+		VALUES (@URL , @Protocol , @Host , @Path , @Status , @Verdict , @ActionTaken , @Score , @Category, @DeviceId)
+	END
+END
+GO;
+
+CREATE PROCEDURE addRuleHit
+	@RuleId INT,
+	@ThreatEventId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	INSERT INTO dbo.RuleHits (RuleId , ThreatEventId) VALUES (@RuleId , @ThreatEventId)
+END
+GO;
+
+CREATE PROCEDURE addDetectionReason
+	@ReasonCode NVARCHAR(50),
+	@Weight INT,
+	@Details NVARCHAR(255),
+	@ThreatEventId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO  dbo.DetectionReasons (ReasonCode , Weight , Details , ThreatEventId) 
+	VALUES (@ReasonCode , @Weight , @Details , @ThreatEventId)
+END
+GO;
+
+CREATE PROCEDURE addAlert
+	@UserId INT,
+	@DeviceId INT,
+	@ThreatEventId INT,
+	@Severity NVARCHAR(50),
+    @IsRead BIT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO dbo.Alerts (UserId , DeviceId , ThreatEventId , Severity , IsRead)
+	VALUES (@UserId , @DeviceId , @ThreatEventId , @Severity , @IsRead)
+END
+GO;
+
+
