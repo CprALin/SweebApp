c60c32f174d62b31bbc2ca5a9dbbff916a1074a0
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2026-02-14 15:38:43 +0200

    # Add SQL Server database connection via AppDbContext

--- SweebAppAPIs/Program.cs
+++ SweebAppAPIs/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SweebAppAPIs.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Add Databse Context for connecting to SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
 
 var app = builder.Build();
 
